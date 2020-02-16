696cab9b4610e47b5c8f073a7fb53282c64934ca
Aron <zhumagulov-00-00@mail.ru>
2020-02-16 14:25:06 -0600

    yes, i do it

---
+++ numbersAndMath/src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println( "I will now count my chickens:" );
+
+        System.out.println( "Hens " +  (25.0 + 30.0 )/ 6.0 %1);
+//        I count hens and division by six
+        System.out.println( "Roosters " + ( (100.0 - 25.0) * (3.0 / 4.0 %0.1 )));
+//        I subtract from one hundred twenty five and multiply by three and look for 4%
+        System.out.println( "Now I will count the eggs:" );
+//        I three plus two and one minus five
+        System.out.println(((3.0 + 2.0 + 1.0 - 5.0 + 4.0) / 2.0 %1) - (1.0 / (4.0 + 6.0 %0.1)));
+        System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
+//        comparing
+        System.out.println( 3 + 2 < 5 - 7 );
+//        comparing falls
+        System.out.println( "What is 3 + 2? " + ( 3 + 2 ) );
+//        3 + 2 = 5
+        System.out.println( "What is 5 - 7? " + ( 5 - 7 ) );
+//        5 - 7 = -2
+        System.out.println( "Oh, that's why it's false." );
+//        yeah i know
+        System.out.println( "How about some more." );
+//        so... let's sea
+        System.out.println( "Is it greater? 5 > -2 " + ( 5 > -2 ) );
+//        comparing true
+        System.out.println( "Is it greater or equal? 5 >= -2 " + ( 5 >= -2 ) );
+//        comparing true
+        System.out.println( "Is it less or equal? 5 <= -2 " + ( 5 <= -2 ) );
+//        comparing falls
+    }
+}
