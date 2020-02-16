5378f89d58cdb8e883a1aaabad9c156d7aa7b860
Aron <zhumagulov-00-00@mail.ru>
2020-02-16 12:31:47 -0600

    my initials

---
+++ initials/src/main/java/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("For the name Zhumagulov Aron Talgatovich....");
+
+        System.out.println("ZZZZZ    A    TTTTT");
+        System.out.println("    Z   A A     T  ");
+        System.out.println("   Z   A   A    T  ");
+        System.out.println("  Z    AAAAA    T  ");
+        System.out.println(" Z     A   A    T  ");
+        System.out.println("Z      A   A    T  ");
+        System.out.println("ZZZZZ  A   A    T  ");
+    }
+}
