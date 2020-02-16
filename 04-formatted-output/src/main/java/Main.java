5bc0cc03021de058ca8e7415a7ccf14666997fd7
Aron <zhumagulov-00-00@mail.ru>
2020-02-15 23:21:56 -0600

    I say hello

---
+++ 04-formatted-output/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Person person1 = new Person("Aron", 19);
+
+
+        System.out.println("My name is " + person1.getName() + " and I am " + person1.getAge() + " years old");
+    }
+}
