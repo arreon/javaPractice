bf8711b2036556a07ec1f292a96dc7c68d1261c4
Aron <zhumagulov-00-00@mail.ru>
2020-02-15 22:39:50 -0600

    write mail for my fried

---
+++ 01-a-chatty-program/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("----------------------------------");
+        System.out.println("HI!");
+        System.out.println("How are you?");
+        System.out.println("I'm fine!");
+        System.out.println("Doing really well in fact...");
+        System.out.println("Isn't it a beautiful day?");
+        System.out.println("Smiling is my favorite!");
+        System.out.println("Good bye!!!");
+        System.out.println("----------------------------------");
+    }
+}
