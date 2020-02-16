ef9068b1a707d777f0cc51c36cb8db255d16ec69
Aron <zhumagulov-00-00@mail.ru>
2020-02-16 16:01:14 -0600

    road way

---
+++ 01-module/02-lesson/rectungular-paving-company/src/main/java/RectPavingCompany.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RectPavingCompany {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the width of the driveway in feet?");
+        int width = Integer.parseInt(scan.nextLine());
+
+        System.out.println("what is the length of the driveway in feet");
+        int length = Integer.parseInt(scan.nextLine());
+
+        int area = width * length;
+        int perimeter = (length + width) * 2;
+
+        float cementCost = 12.5f;
+        float framingCost = 8.25f;
+
+        System.out.println("the width is " + width + " feet");
+        System.out.println("the length is " + length + " feet");
+        System.out.format("the area of the driveway is %d square feet. %n", area);
+        System.out.format("the perimeter of the driveway is %d square feet. %n", perimeter);
+
+        System.out.format("the cost of the cement is %f. %n", area * cementCost);
+        System.out.format("the cost of the framing is %f. %n", perimeter * framingCost);
+
+//        System.out.format("the c");
+    }
+}
